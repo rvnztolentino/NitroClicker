66a47cb2675da418daba505a41822578f6ae220e
Renz Tolentino <johnrenztolentino1028@gmail.com>
2024-10-13 17:34:05 +0800

    updated maximum rate to 100

--- NitroClicker/src/sleep_duration.cpp
+++ NitroClicker/src/sleep_duration.cpp
@@ -9,16 +9,27 @@ int sleepDuration() {
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(h, 10);
 	do {
-		std::cout << "Select your clicks per second (CPS) from 1 to 50: ";
+		std::cout << "Select your clicks per second (CPS): ";
 		std::cin >> CPS;
-
-		if (CPS >= 1 && CPS <= 50) {
+		if (CPS >= 1 && CPS <= 100) {
 			std::cout << "Selected CPS: " << CPS << "\n\n";
 			exitFlag = true;
 		}
 
+		else if (CPS > 100) {
+			CPS = 100; // Set to Max (100 CPS) if user intended rate exceeds
+			std::cout << "Selected CPS: " << CPS << " (MAX)" << "\n\n";
+			exitFlag = true;
+		}
+
+		else if (CPS < 1) { // Set to Min (1 CPS) if user intended rate exceeds
+			CPS = 1;
+			std::cout << "Selected CPS: " << CPS << " (MIN)" << "\n\n";
+			exitFlag = true;
+		}
+
 		else {
-			std::cout << "Please choose from 1 to 50 only. Try again.\n\n";
+			std::cout << "\n\n";
 		}
 	} while (!exitFlag);
 
